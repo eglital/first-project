340bef0ce9ad5b6e693c22a4c27aa85944eb221c
Egle Libby <egle.ciparyte@gmail.com>
2016-09-29 05:17:22 +0000

    uses html file

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 8080;
+var api = require("./myapi.js");
 
-app.listen(port, function(){})
+app.use(express.static('public'));
+
+app.listen(port, function(){});
